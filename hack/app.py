df234bea7728e383e5cd87a5caf05651176d2557
Adam <fergie57@btinternet.com>
2015-03-28 14:37:38 +0000

    average positivity index between 1 and minus 1

--- hack/app.py
+++ hack/app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 
 app.debug = True
 
+positivity = []
+global_average_positivity = 0
+
 
 class ProcessingThread(threading.Thread):
 
@@ -17,15 +20,38 @@ class ProcessingThread(threading.Thread):
         self.search_term = search_term
 
     def run(self):
+        global positivity
         c = tweet_collector.Collect()
         c.connect()
         for tweet in c.stream(self.search_term):
-            pass
+                blob = TextBlob(tweet["text"])
+
+                average_positivity = []
+                total_positivity = 0
+
+                for sentence in blob.sentences:
+                    average_positivity.append(sentence.sentiment.polarity)
+
+                for value in average_positivity:
+                    total_positivity += value
+
+                ret_val = total_positivity/len(average_positivity)
+
+
+                positivity.append(ret_val)
+
+
 
 
 @app.route('/')
 def hello(name=None):
-    return render_template('hello.html', name=random.randint(1,1000))
+    global positivity
+
+    for value in positivity:
+        global_average_positivity += value
+        global_average_positivity = global_average_positivity/len(positivity)
+
+    return render_template('hello.html', name=random.randint(1,1000), global_average_positivity=global_average_positivity)
 
 if __name__ == '__main__':
     app.run()
